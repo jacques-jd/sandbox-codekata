1a68ed72ca7d125fb4b8976c909dddf41057a4f7
Jacques Dancause <jacques.dancause@ssc-spc.gc.ca>
2024-08-09 16:02:12 -0300

    sumOfArray challenge added

---
+++ sandbox.js
@@ -0,0 +1,12 @@
+const sumOfArray = (array) => {
+  // your code goes here.
+  return 0;
+};
+
+let result1 = sumOfArray([15, 10, 5, 2]);
+let result2 = sumOfArray([1, 2, 3]);
+let result3 = sumOfArray([1, 99, 300]);
+
+console.log(result1 === 32 ? "Result 1 is good" : "Result 1 is not correct");
+console.log(result2 === 6 ? "Result 2 is good" : "Result 2 is not correct");
+console.log(result3 === 400 ? "Result 3 is good" : "Result 3 is not correct");
